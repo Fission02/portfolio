4f5c43824767e45f38e253811662bc2e4e14cef1
fissionClone <vipulyadav503@gmail.com>
2024-01-17 20:32:36 +0530

    This is my portfolio

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+// nav section 
+function ToggleMenu() {
+    var menuList = document.querySelector('.menu-list');
+    var container = document.querySelector(`.container`);
+    container.classList.toggle("change");
+    menuList.classList.toggle('open');   
+}
+
+// first scetion 
+
